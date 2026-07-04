dd24fa1673e82ba531c35014860deffdd13e3d93
Евгений Шмаров <bablvii@yandex.ru>
2026-07-04 13:43:03 +0700

    Добавление сброса статистики ссылки

--- app/Filament/Resources/ShortLinkResource/Pages/ViewShortLink.php
+++ app/Filament/Resources/ShortLinkResource/Pages/ViewShortLink.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\ShortLinkResource\Pages;
 
 use App\Filament\Resources\ShortLinkResource;
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\ViewRecord;
 
 class ViewShortLink extends ViewRecord
@@ -18,6 +19,24 @@ class ViewShortLink extends ViewRecord
     protected function getHeaderActions(): array
     {
         return [
+            Actions\Action::make('resetStats')
+                ->label('Сбросить статистику')
+                ->color('danger')
+                ->icon('heroicon-o-arrow-path')
+                ->requiresConfirmation()
+                ->modalHeading('Сбросить статистику ссылки')
+                ->modalDescription('Все переходы по этой ссылке будут удалены. Сама ссылка останется доступной.')
+                ->modalSubmitActionLabel('Сбросить')
+                ->action(function (): void {
+                    $this->record->clicks()->delete();
+
+                    Notification::make()
+                        ->title('Статистика сброшена')
+                        ->success()
+                        ->send();
+
+                    $this->redirect(static::getResource()::getUrl('view', ['record' => $this->record]));
+                }),
             Actions\EditAction::make()
                 ->label('Изменить'),
             Actions\DeleteAction::make()
